ded7203e1eea92a00eac2e17e9b0b5f8ffc4738f
Alex Blauth <thomas98alex@live.com>
2019-03-11 18:43:06 -0400

    edited confiig files to work with heroku

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,6 @@ module.exports = {
   db: {
     uri: 'mongodb://alex:alexuf1@ds221115.mlab.com:21115/alexblauth', //place the URI of your mongo database here.
   },
-  port: 8080
+  //port: 8080
+  port: process.env.PORT
 };
