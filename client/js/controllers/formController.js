5ae01b2b85797f9602c3ea202919d48fd734feff
ajlehenbauer <ajlehenbauer@ufl.edu>
2019-03-30 13:47:47 -0400

    -

--- client/js/controllers/formController.js
+++ client/js/controllers/formController.js
@@ -1,11 +1,25 @@
 // https://www.solodev.com/blog/web-design/how-to-hide-form-fields-based-upon-user-selection.stml
 
+
+$("#JobSelect").change(function() {
+  if ($(this).val() == "other") {
+    $('#jobOther').show();
+    
+  } else {
+    $('#jobOther').hide();
+    
+  }
+});
+$("#JobSelect").trigger("change");
+
+
 $("#mentorCheck").change(function() {
     if ($(this).val() == "yes") {
-      $('#mentorStrength').show();
+      $('#allStrength').show();
       
     } else {
-      $('#mentorStrength').hide();
+      $('#allStrength').hide();
+
       
     }
   });
@@ -14,11 +28,10 @@ $("#mentorCheck").change(function() {
 
   $("#mentorStrength1check").change(function() {
     if ($(this).val() == "Strength1empty") {
-      $('#mentorStrength2').hide();
+      $('#strength2').hide();
       
     } else {
-      $('#mentorStrength2').show();
-      
+      $('#strength2').show();
     }
   });
   $("#mentorStrength1check").trigger("change");
@@ -34,3 +47,37 @@ $("#mentorCheck").change(function() {
     }
   });
   $("#mentorStrength2check").trigger("change");
+
+  $("#menteeCheck").change(function() {
+    if ($(this).val() == "yes") {
+      $('#allGoal').show();
+      
+    } else {
+      $('#allGoal').hide();
+
+      
+    }
+  });
+  $("#menteeCheck").trigger("change");
+
+  $("#menteeGoal1check").change(function() {
+    if ($(this).val() == "Goal1empty") {
+      $('#goal2').hide();
+      
+    } else {
+      $('#goal2').show();
+    }
+  });
+  $("#menteeGoal1check").trigger("change");
+
+
+  $("#menteeGoal2check").change(function() {
+    if ($(this).val() == "Goal2empty") {
+      $('#menteeGoal3').hide();
+      
+    } else {
+      $('#menteeGoal3').show();
+      
+    }
+  });
+  $("#menteeGoal2check").trigger("change");
