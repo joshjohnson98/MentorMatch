0436d70f9d158b8e9e9339b62f3a66c3e12a9c8d
Joshua Johnson <jjohnson316@ufl.edu>
2019-04-06 18:12:07 -0400

    Removed showDetails function. Removed some console logs. User profile info now populates form if user is logs in and clicks some form attribute. Fix this to fill automatically later...

--- client/js/controllers/controller.js
+++ client/js/controllers/controller.js
@@ -115,14 +115,7 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
               }, function(error) {
                 $scope.error = 'Unable to add lyour person isting!' + error;
               });
-
-
-
     };
-
-
-
-
 
 
     $scope.deleteListing = function(id) {
@@ -134,14 +127,9 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       });
     };
 
-    $scope.showDetails = function(index) {
-      $scope.detailedInfo = $scope.listings[index];
-    };
-
     $scope.login = function(emailParam){
       $scope.detailedInfo.email = emailParam; //Set currentUserEmail scope variable
 
-      console.log("We are in angular login function now!");
       console.log("Email passed in: " + emailParam);
 
       var emailAlreadyInDB = false;
@@ -151,24 +139,104 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
         if(value.email == emailParam){
           
           emailAlreadyInDB = true;
-          console.log("Found email in database!");
+          console.log("User found. Email already in DB");
+          //If email is already in database, show user information in profile form
+          $scope.detailedInfo.name=value.name;
+          $scope.detailedInfo.ethnicity.value=value.ethnicity.value;
+          $scope.detailedInfo.ethnicity.score=value.ethnicity.score;
+          $scope.detailedInfo.sexualOrientation.value=value.sexualOrientation.value;
+          $scope.detailedInfo.sexualOrientation.value=value.sexualOrientation.score;
+          $scope.detailedInfo.gender.value=value.gender.value;
+          $scope.detailedInfo.gender.score=value.gender.score;
+          $scope.detailedInfo.industry.value=value.industry.value;
+          $scope.detailedInfo.industry.score=value.industry.score;
+          $scope.detailedInfo.bio=value.bio;
+          $scope.detailedInfo.isMentor=value.isMentor;
+          $scope.detailedInfo.isMentee=value.isMentee;
+          $scope.detailedInfo.mentorStrength1.value=value.mentorStrength1.value;
+          $scope.detailedInfo.mentorStrength1.score=value.mentorStrength1.score;
+          $scope.detailedInfo.mentorStrength2.value=value.mentorStrength2.value;
+          $scope.detailedInfo.mentorStrength2.score=value.mentorStrength2.score;
+          $scope.detailedInfo.mentorStrength3.value=value.mentorStrength3.value;
+          $scope.detailedInfo.mentorStrength3.score=value.mentorStrength3.score;
+          $scope.detailedInfo.menteeGoal1.value=value.menteeGoal1.value;
+          $scope.detailedInfo.menteeGoal1.score=value.menteeGoal1.score;
+          $scope.detailedInfo.menteeGoal2.value=value.menteeGoal2.value;
+          $scope.detailedInfo.menteeGoal2.score=value.menteeGoal2.score;
+          $scope.detailedInfo.menteeGoal3.value=value.menteeGoal3.value;
+          $scope.detailedInfo.menteeGoal3.score=value.menteeGoal3.score;
+          $scope.detailedInfo.location.score=value.location.score;
+          $scope.detailedInfo.location.city=value.location.city;
+          $scope.detailedInfo.location.state=value.location.state;
+          $scope.detailedInfo.location.country=value.location.country;
+          $scope.detailedInfo.language.value=value.language.value;
         }
       })
 
-      //If email is already in database, show user information
-      if(emailAlreadyInDB){
-        console.log("Email already in DB");
-
-        //*****TODO******
-
-
-      }else{  //ELSE, create new user
+      
+      if(!emailAlreadyInDB){
         console.log("Email not found in DB. Adding listing now...");
-        var listing = {
-          email: emailParam //Initialize email attribute
-                 //All other attributes are left undefined
-        };
-        $scope.addListing($scope.detailedInfo);
+        var newListing = {
+          email: emailParam,
+          name:{
+            value:"",
+            score:0
+          },
+          ethnicity:{
+            value:"",
+            score:0
+          },
+          sexualOrientation:{
+            value:"",
+            score:0
+          },
+          gender:{
+            value:"",
+            score:0
+          },
+          industry:{
+            value:"",
+            score:0
+          },
+          bio:"",
+          isMentor:"",
+          mentorStrength1:{
+            value:"",
+            score:0
+          },
+          mentorStrength2:{
+            value:"",
+            score:0
+          },
+          mentorStrength3:{
+            value:"",
+            score:0
+          },
+          isMentee:"",
+          menteeGoal1:{
+            value:"",
+            score:0
+          },
+          menteeGoal2:{
+            value:"",
+            score:0
+          },
+          menteeGoal3:{
+            value:"",
+            score:0
+          },
+          language:{
+            value:"",
+            score:0
+          },
+          location:{
+            country:"",
+            state:"",
+            city:"",
+            score:0
+          }
+        }
+        $scope.addListing(newListing);
       }
     }
 
@@ -205,7 +273,6 @@ function attachSignin(element)
     auth2.attachClickHandler(element, {},
             function (googleUser)
             {
-                console.log("Button clicked!\n Email: " + googleUser.getBasicProfile().getEmail());
                 var email = googleUser.getBasicProfile().getEmail();  //Retrieve current user email
                 //var nameuser = googleUser.getBasicProfile().getName();  //Retrieve current user email
                 angular.element($('#MainWrap')).scope().login(email); //Pass email into angular function (login)
