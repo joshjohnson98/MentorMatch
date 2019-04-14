0f25e9c87840698a9b060689d10f57f63cdf45d6
anhhoang <anhhoang@ufl.edu>
2019-04-14 19:32:55 -0400

    fix some bugs, still have the problem that there is a button that is not connected to app.js

--- client/js/controllers/controller.js
+++ client/js/controllers/controller.js
@@ -291,7 +291,6 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   }
 ]);
 
-
 window.onload = function() {
 
   console.log("local storage has: " + localStorage.getItem('useremail'));
@@ -300,9 +299,10 @@ window.onload = function() {
   console.log("the email when load is: " + localStorage.getItem("useremail"));
 
   angular.element($('#MainWrap')).scope().detailedInfo.email = localStorage.getItem('useremail');
-  // angular.element($('#MainWrap')).scope().login(localStorage["useremail"],localStorage["username"]);
+  angular.element($('#MainWrap')).scope().detailedInfo.name = localStorage.getItem('username');
   if(localStorage.getItem('useremail')!=null){
-    angular.element($('#MainWrap')).scope().$apply( angular.element($('#MainWrap')).scope().login(localStorage.getItem('useremail'), localStorage.getItem('username'))); //Pass email into angular function (login)
+    angular.element($('#MainWrap')).scope().$apply( angular.element($('#MainWrap')).scope().
+    login(localStorage.getItem('useremail'), localStorage.getItem('username'))); //Pass email into angular function (login)
   }
 };
 
@@ -337,133 +337,133 @@ function attachSignin(element)
                 alert(JSON.stringify(error, undefined, 2));
             });
 }
-
-var loginPage = function($scope)
-{
-  $scope.login = function (emailParam, nameParam)
-  {
-    $scope.detailedInfo.email = emailParam; //Set currentUserEmail scope variable
-    $scope.detailedInfo.name.value = nameParam;
-
-
-    localStorage.setItem('useremail', emailParam);
-    localStorage['useremail'] = emailParam;
-
-
-    console.log("We are in angular login function now!");
-
-    console.log("Email passed in: " + emailParam);
-    console.log("Name passed in: " + nameParam);
-
-    var emailAlreadyInDB = false;
-
-
-    angular.forEach($scope.listings, function (value, key)
-    {
-      if (value.email == emailParam)
-      {
-
-        emailAlreadyInDB = true;
-        console.log("User found. Email already in DB");
-        //If email is already in database, show user information in profile form
-        $scope.detailedInfo.ethnicity.value = value.ethnicity.value;
-        $scope.detailedInfo.ethnicity.score = value.ethnicity.score;
-        $scope.detailedInfo.sexualOrientation.value = value.sexualOrientation.value;
-        $scope.detailedInfo.sexualOrientation.value = value.sexualOrientation.score;
-        $scope.detailedInfo.gender.value = value.gender.value;
-        $scope.detailedInfo.gender.score = value.gender.score;
-        $scope.detailedInfo.industry.value = value.industry.value;
-        $scope.detailedInfo.industry.score = value.industry.score;
-        $scope.detailedInfo.bio = value.bio;
-        $scope.detailedInfo.isMentor = value.isMentor;
-        $scope.detailedInfo.isMentee = value.isMentee;
-        $scope.detailedInfo.mentorStrength1.value = value.mentorStrength1.value;
-        $scope.detailedInfo.mentorStrength1.score = value.mentorStrength1.score;
-        $scope.detailedInfo.mentorStrength2.value = value.mentorStrength2.value;
-        $scope.detailedInfo.mentorStrength2.score = value.mentorStrength2.score;
-        $scope.detailedInfo.mentorStrength3.value = value.mentorStrength3.value;
-        $scope.detailedInfo.mentorStrength3.score = value.mentorStrength3.score;
-        $scope.detailedInfo.menteeGoal1.value = value.menteeGoal1.value;
-        $scope.detailedInfo.menteeGoal1.score = value.menteeGoal1.score;
-        $scope.detailedInfo.menteeGoal2.value = value.menteeGoal2.value;
-        $scope.detailedInfo.menteeGoal2.score = value.menteeGoal2.score;
-        $scope.detailedInfo.menteeGoal3.value = value.menteeGoal3.value;
-        $scope.detailedInfo.menteeGoal3.score = value.menteeGoal3.score;
-        $scope.detailedInfo.location.score = value.location.score;
-        $scope.detailedInfo.location.city = value.location.city;
-        $scope.detailedInfo.location.state = value.location.state;
-        $scope.detailedInfo.location.country = value.location.country;
-        $scope.detailedInfo.language.value = value.language.value;
-      }
-    })
-
-
-    if (!emailAlreadyInDB)
-    {
-      console.log("Email not found in DB. Adding listing now...");
-      var newListing = {
-        email: emailParam,
-        name: {
-          value: nameParam,
-          score: 0
-        },
-        ethnicity: {
-          value: "",
-          score: 0
-        },
-        sexualOrientation: {
-          value: "",
-          score: 0
-        },
-        gender: {
-          value: "",
-          score: 0
-        },
-        industry: {
-          value: "",
-          score: 0
-        },
-        bio: "",
-        isMentor: "",
-        mentorStrength1: {
-          value: "",
-          score: 0
-        },
-        mentorStrength2: {
-          value: "",
-          score: 0
-        },
-        mentorStrength3: {
-          value: "",
-          score: 0
-        },
-        isMentee: "",
-        menteeGoal1: {
-          value: "",
-          score: 0
-        },
-        menteeGoal2: {
-          value: "",
-          score: 0
-        },
-        menteeGoal3: {
-          value: "",
-          score: 0
-        },
-        language: {
-          value: "",
-          score: 0
-        },
-        location: {
-          country: "",
-          state: "",
-          city: "",
-          score: 0
-        }
-      }
-      $scope.addListing(newListing);
-    }
-  }
-}
+//
+// var loginPage = function($scope)
+// {
+//   $scope.login = function (emailParam, nameParam)
+//   {
+//     $scope.detailedInfo.email = emailParam; //Set currentUserEmail scope variable
+//     $scope.detailedInfo.name.value = nameParam;
+//
+//
+//     localStorage.setItem('useremail', emailParam);
+//     localStorage['useremail'] = emailParam;
+//
+//
+//     console.log("We are in angular login function now!");
+//
+//     console.log("Email passed in: " + emailParam);
+//     console.log("Name passed in: " + nameParam);
+//
+//     var emailAlreadyInDB = false;
+//
+//
+//     angular.forEach($scope.listings, function (value, key)
+//     {
+//       if (value.email == emailParam)
+//       {
+//
+//         emailAlreadyInDB = true;
+//         console.log("User found. Email already in DB");
+//         //If email is already in database, show user information in profile form
+//         $scope.detailedInfo.ethnicity.value = value.ethnicity.value;
+//         $scope.detailedInfo.ethnicity.score = value.ethnicity.score;
+//         $scope.detailedInfo.sexualOrientation.value = value.sexualOrientation.value;
+//         $scope.detailedInfo.sexualOrientation.value = value.sexualOrientation.score;
+//         $scope.detailedInfo.gender.value = value.gender.value;
+//         $scope.detailedInfo.gender.score = value.gender.score;
+//         $scope.detailedInfo.industry.value = value.industry.value;
+//         $scope.detailedInfo.industry.score = value.industry.score;
+//         $scope.detailedInfo.bio = value.bio;
+//         $scope.detailedInfo.isMentor = value.isMentor;
+//         $scope.detailedInfo.isMentee = value.isMentee;
+//         $scope.detailedInfo.mentorStrength1.value = value.mentorStrength1.value;
+//         $scope.detailedInfo.mentorStrength1.score = value.mentorStrength1.score;
+//         $scope.detailedInfo.mentorStrength2.value = value.mentorStrength2.value;
+//         $scope.detailedInfo.mentorStrength2.score = value.mentorStrength2.score;
+//         $scope.detailedInfo.mentorStrength3.value = value.mentorStrength3.value;
+//         $scope.detailedInfo.mentorStrength3.score = value.mentorStrength3.score;
+//         $scope.detailedInfo.menteeGoal1.value = value.menteeGoal1.value;
+//         $scope.detailedInfo.menteeGoal1.score = value.menteeGoal1.score;
+//         $scope.detailedInfo.menteeGoal2.value = value.menteeGoal2.value;
+//         $scope.detailedInfo.menteeGoal2.score = value.menteeGoal2.score;
+//         $scope.detailedInfo.menteeGoal3.value = value.menteeGoal3.value;
+//         $scope.detailedInfo.menteeGoal3.score = value.menteeGoal3.score;
+//         $scope.detailedInfo.location.score = value.location.score;
+//         $scope.detailedInfo.location.city = value.location.city;
+//         $scope.detailedInfo.location.state = value.location.state;
+//         $scope.detailedInfo.location.country = value.location.country;
+//         $scope.detailedInfo.language.value = value.language.value;
+//       }
+//     })
+//
+//
+//     if (!emailAlreadyInDB)
+//     {
+//       console.log("Email not found in DB. Adding listing now...");
+//       var newListing = {
+//         email: emailParam,
+//         name: {
+//           value: nameParam,
+//           score: 0
+//         },
+//         ethnicity: {
+//           value: "",
+//           score: 0
+//         },
+//         sexualOrientation: {
+//           value: "",
+//           score: 0
+//         },
+//         gender: {
+//           value: "",
+//           score: 0
+//         },
+//         industry: {
+//           value: "",
+//           score: 0
+//         },
+//         bio: "",
+//         isMentor: "",
+//         mentorStrength1: {
+//           value: "",
+//           score: 0
+//         },
+//         mentorStrength2: {
+//           value: "",
+//           score: 0
+//         },
+//         mentorStrength3: {
+//           value: "",
+//           score: 0
+//         },
+//         isMentee: "",
+//         menteeGoal1: {
+//           value: "",
+//           score: 0
+//         },
+//         menteeGoal2: {
+//           value: "",
+//           score: 0
+//         },
+//         menteeGoal3: {
+//           value: "",
+//           score: 0
+//         },
+//         language: {
+//           value: "",
+//           score: 0
+//         },
+//         location: {
+//           country: "",
+//           state: "",
+//           city: "",
+//           score: 0
+//         }
+//       }
+//       $scope.addListing(newListing);
+//     }
+//   }
+// }
 
 startApp();
