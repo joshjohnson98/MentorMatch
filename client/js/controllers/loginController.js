4fe94f1163e27e95aa911d2c58bc2f50c38c6019
anhhoang <anhhoang@ufl.edu>
2019-04-14 19:06:30 -0400

    switched 2 pages, update the information in the profile

---
+++ client/js/controllers/loginController.js
@@ -0,0 +1,35 @@
+
+
+var googleUser = {};
+var startApp = function() {
+    gapi.load('auth2', function(){
+        // Retrieve the singleton for the GoogleAuth library and set up the client.
+        auth2 = gapi.auth2.init({
+            client_id: '94097431082-u080o437bpmes48td7lh5ojcungl8hsn.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+        });
+        attachSignin(document.getElementById('customBtn'));
+    });
+};
+
+
+
+function attachSignin(element)
+{
+    auth2.attachClickHandler(element, {},
+        function (googleUser)
+        {
+            localStorage.clear();
+            var email = googleUser.getBasicProfile().getEmail();  //Retrieve current user email
+            var name = googleUser.getBasicProfile().getName();  //Retrieve current user email
+            // angular.element($('#MainWrap')).scope().login(email, name); //Pass email into angular function (login)
+            window.location.href = "index.html";
+            localStorage.setItem("useremail", email);
+            localStorage.setItem("username", name);
+        }, function (error)
+        {
+            alert(JSON.stringify(error, undefined, 2));
+        });
+}
+
+startApp();
