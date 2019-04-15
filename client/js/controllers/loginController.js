6175897b09a8667ed9875f8e201f66f17de43e06
anhhoang <anhhoang@ufl.edu>
2019-04-14 20:33:16 -0400

    fixed the auto load bug

--- client/js/controllers/loginController.js
+++ client/js/controllers/loginController.js
@@ -3,6 +3,7 @@
 var googleUser = {};
 var startApp = function() {
     gapi.load('auth2', function(){
+        console.log("bla in hereee");
         // Retrieve the singleton for the GoogleAuth library and set up the client.
         auth2 = gapi.auth2.init({
             client_id: '94097431082-u080o437bpmes48td7lh5ojcungl8hsn.apps.googleusercontent.com',
@@ -12,20 +13,20 @@ var startApp = function() {
     });
 };
 
-
-
 function attachSignin(element)
 {
     auth2.attachClickHandler(element, {},
         function (googleUser)
         {
+            console.log("bla in bla");
+            // alert("do so");
             localStorage.clear();
             var email = googleUser.getBasicProfile().getEmail();  //Retrieve current user email
             var name = googleUser.getBasicProfile().getName();  //Retrieve current user email
-            // angular.element($('#MainWrap')).scope().login(email, name); //Pass email into angular function (login)
-            window.location.href = "index.html";
             localStorage.setItem("useremail", email);
             localStorage.setItem("username", name);
+            // angular.element($('#MainWrap')).scope().login(email, name); //Pass email into angular function (login)
+            window.location.href = "index.html";
         }, function (error)
         {
             alert(JSON.stringify(error, undefined, 2));
